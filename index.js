41b0e064415477b1535254ed564417daa7b74857
WhyK <windchimeyk@gmail.com>
2020-03-20 21:56:41 +0900

    update: DAMMY_DATAを修正

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const { send } = require('micro')
 const { router, get } = require('microrouter')
 
-const DAMMY_DATA = [
-  {id: 1, name: 'Steven Paul "Steve" Jobs', founding: 'Apple Inc.'},
-  {id: 2, name: 'Stephen Gary Wozniak', founding: 'Apple Inc.'},
-  {id: 3, name: 'William Henry "Bill" Gates III', founding: 'Microsoft Corporation'},
-  {id: 4, name: 'Paul Gardner Allen', founding: 'Microsoft Corporation'},
-  {id: 5, name: 'Lawrence Edward "Larry" Page', founding: 'Google LLC'},
-  {id: 6, name: 'Sergey Mikhailovich Brin', founding: 'Google LLC'},
-  {id: 7, name: 'Inoue Masahiro', founding: 'Yahoo Japan Corporation'},
-  {id: 8, name: 'Son Masayoshi', founding: 'SoftBank Group Corp.'},
-  {id: 9, name: 'Mikitani Hiroshi', founding: 'Rakuten, Inc.'},
-  {id: 10, name: 'Nishimura Hiroyuki(HIROYUKI)', founding: '2ch.net'},
-]
+const DAMMY_DATA = {
+  data: [
+    {id: 1, name: 'Steven Paul "Steve" Jobs', founding: 'Apple Inc.'},
+    {id: 2, name: 'Stephen Gary Wozniak', founding: 'Apple Inc.'},
+    {id: 3, name: 'William Henry "Bill" Gates III', founding: 'Microsoft Corporation'},
+    {id: 4, name: 'Paul Gardner Allen', founding: 'Microsoft Corporation'},
+    {id: 5, name: 'Lawrence Edward "Larry" Page', founding: 'Google LLC'},
+    {id: 6, name: 'Sergey Mikhailovich Brin', founding: 'Google LLC'},
+    {id: 7, name: 'Inoue Masahiro', founding: 'Yahoo Japan Corporation'},
+    {id: 8, name: 'Son Masayoshi', founding: 'SoftBank Group Corp.'},
+    {id: 9, name: 'Mikitani Hiroshi', founding: 'Rakuten, Inc.'},
+    {id: 10, name: 'Nishimura Hiroyuki(HIROYUKI)', founding: '2ch.net'},
+  ]
+}
 
 const hello = async (_, res) => await send(res, 200, {get: 200})
 
